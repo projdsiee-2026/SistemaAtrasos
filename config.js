efdfdc32cb450b236c79c287790fb84b5be2844e
projdsiee-2026 <projdsiee@gmail.com>
2026-08-13 12:08:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbx9fIg0nC1hp39YJCmeQFsLaM53rrhvvyCys7u03dT2JkDkdn9GVu6rYvXNHOqvd6Mshg/exec",
   SCHOOL_NAME: "Instituto Estadual de Educação Dr. Caetano Munhoz da Rocha",
   
-  LOGO_URL: "assets/logo.png",
+  LOGO_URL: "logo.png",
   SCAN_PAUSE_MS: 2500,
   FOOTER_YEAR: "2026",
   FOOTER_TEAM: "Desenvolvimento de Sistemas IEE",
